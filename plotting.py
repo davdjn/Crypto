d41b4e371fb08d0a2f5119eff8c6933f0e30b78e
davdjn <davdjn@pm.me>
2021-09-27 15:50:12 -0400

    plot_portfolio added

--- plotting.py
+++ plotting.py
@@ -13,12 +13,16 @@ def plot_coins(coins, days, data_resolution=300):
         fig.add_trace(go.Scatter(x=times_utc, y=coin_tools.get_prices(c, days, data_resolution), name=c))
     fig.show()
     
-def plot_history_value(portfolio):
+def plot_portfolio(portfolio, metric='value'):
+    key = {'amount' : 1, 'price' : 2, 'value' : 3}
     data = portfolio.pivot_history()
+    sorted_coins = sorted(portfolio.coins, key=lambda c: data[c][key[metric]][-1], reverse=True)
+    
     fig = go.Figure()
-    fig.update_layout()
-    fig.add_trace(go.Scatter(x=data['total'][0], y=data['total'][1], name='total value'))
-    for c in portfolio.coins:
+    fig.update_layout(title=metric)
+    if key[metric] == 3:
+        fig.add_trace(go.Scatter(x=data['total'][0], y=data['total'][1], name='total'))
+    for c in sorted_coins:
         temp = data[c]
-        fig.add_trace(go.Scatter(x=temp[0], y=temp[3], name=c+' value'))
+        fig.add_trace(go.Scatter(x=temp[0], y=temp[key[metric]], name=c))
     fig.show()
